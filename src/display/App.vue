5f23bcfab624dd8c1506e6378a047c2503b39901
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-11-24 15:59:48 -0500

    :sparkles: integrating open weather map API and displaying weather data within component

--- src/display/App.vue
+++ src/display/App.vue
@@ -2,18 +2,21 @@
   <div>
     <date-time></date-time>
     <cat-fact></cat-fact>
+    <weather-widget></weather-widget>
   </div>
 </template>
 
 <script>
 import CatFact from './components/CatFact';
 import DateTime from './components/DateTime';
+import Weather from './components/Weather';
 
 export default {
   name: 'App',
   components: {
     'cat-fact': CatFact,
     'date-time': DateTime,
+    'weather-widget': Weather,
   },
 };
 </script>
