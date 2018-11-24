2b0f3a3cced1c8c1916955b26bc5c73746864d96
Britta Rodenbeck <rodenbeck.britta@gmail.com>
2018-11-24 15:12:10 -0500

    :sparkles: adding cat fact api integration and displaying random cat fact

---
+++ src/display/components/CatFact.vue
@@ -0,0 +1,21 @@
+<template>
+  <p v-show="fact.length > 0">🐱 {{ fact }} 🐱</p>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'CatFact',
+  data() {
+    return {
+      fact: '',
+    };
+  },
+  created() {
+    axios.get('https://catfact.ninja/fact')
+      .then(response => this.fact = response.data.fact)
+      .catch(error => console.log(error));
+  },
+};
+</script>
